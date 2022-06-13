041c02bcc50f38b79723896a0da52a7f0d2979aa
Anna Radchenko <anrd.rs>
2022-06-13 18:53:02 +0300

    MarvelServices created to communicate with API

---
+++ src/services/MarvelService.js
@@ -0,0 +1,17 @@
+class MarvelService {
+    getRecource = async (url) => {
+        let res = await fetch(url);
+        
+        if (!res.ok) {
+            throw new Error(`Can not fetch ${url}, status : ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getRecource('https://gateway.marvel.com:443/v1/public/characters?apikey=f65f5a21349dce7531dd6bd0db9e3868')
+    }
+}
+
+export default MarvelService;
