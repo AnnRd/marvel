148cb1f4e803a47dded8c540fa2c19661d4ff530
Anna Radchenko <anrd.rs>
2022-06-18 13:48:14 +0300

    obj.id in mapping in CharList updated

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,6 +25,7 @@ class MarvelService {
 
     _transformCharacter = (character) => {
         return {
+            id: character.id,
             name: character.name,
             description: character.description ? `${character.description.slice(0, 210)}...` : 'There`s no description for this character',
             thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
